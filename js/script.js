50ddff112c6a51d3352d1708586777beb42dd8ed
TiKaTiiK <tinatin.nanitashvili@mfg.ge>
2022-06-05 23:02:56 +0400

    Added Honey Website Files

---
+++ js/script.js
@@ -0,0 +1,91 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+
+/* bee animation */
+
+window.onload = function() {
+    var flakes = [];
+    var types = ['🐝','🐝','🐝'];
+    for(var i = 0, len = 240; i < len; i++) {
+      flakes.push('<i>' + types[i%3]  + '</i>');
+    }  document.getElementById('bee').innerHTML = flakes.join('');
+  };
+  
+
+
+/* slider script*/
+
+let sliderWrap = document.querySelector('.slider-wrap');
+let slider = document.querySelector('.slider');
+let clonesWidth;
+let sliderWidth;
+let clones = [];
+let disableScroll = false;
+let scrollPos;
+
+let items = [...document.querySelectorAll('.slider-item')];
+let images = [...document.querySelectorAll('.img-div')];
+
+images.forEach((image, idx) => {
+    image.style.backgroundImage = 'url(./images/'+(idx+1)+'.jpg)';
+})
+
+items.forEach(item => {
+    let clone = item.cloneNode(true);
+    clone.classList.add('clone');
+    slider.appendChild(clone);
+    clones.push(clone);
+    console.log(clones);
+})
+
+var scrollContainer = document.getElementsByClassName("slider-wrap")[0];
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
+
+window.addEventListener('resize', onLoad);
+
+function onLoad(){
+}
+onLoad();
+
+
+
+//accordition
+let accordion = document.getElementsByClassName('container');
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active2');
+    })
+}
